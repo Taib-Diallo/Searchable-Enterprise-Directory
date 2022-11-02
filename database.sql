ef09e1b66430a4b9a60d88c45bb211e6548e01a3
Taib-Diallo <taiboudiallo225@gmail.com>
2022-11-02 13:52:47 -0400

    Updated table schema to show role access

--- database.sql
+++ database.sql
@@ -22,20 +22,22 @@ values ('Carlos Garcia','655-423-8767', 'employee', 'Hartford', 75000),
 		
 		
 --creating Users tables 
+drop table if exists users;
 create table users (
 	Employee_no int PRIMARY KEY,
 	Name varchar (45) not null,
 	Username varchar(45) not null,
-	Password varchar (60) not null
+	Password varchar (60) not null,
+	Role int not null
 );
 
 -- visualizing the table 
 select * from users;
 
 -- adding data into users table
-insert into users (Employee_no, Name, Username, Password)
-values (1, 'John Smith', 'jsmith@travelers.com', 'dog'), 
-		(2, 'Jackie Hall', 'jhall@travelers.com', 'cat'),
-		(3, 'Ashley Bell', 'abell@travelers.com', 'fish'),
-		(4, 'Carlos Garcia', 'cgarcia@travelers.com', 'snake'),
-		(5, 'Jack Don', 'jdon@travelers.com', 'bird');
+insert into users (Employee_no, Name, Username, Password, Role)
+values (1, 'John Smith', 'jsmith@travelers.com', 'dog', 0), 
+		(2, 'Jackie Hall', 'jhall@travelers.com', 'cat', 1),
+		(3, 'Ashley Bell', 'abell@travelers.com', 'fish', 2),
+		(4, 'Carlos Garcia', 'cgarcia@travelers.com', 'snake', 0),
+		(5, 'Jack Don', 'jdon@travelers.com', 'bird',0);
